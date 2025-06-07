1b42c859aa3feb75c7ac20a8008fa2fcaab4d86a
vikas-1321 <vikky805061@gmil.com>
2025-06-07 21:56:53 +0530

    this is my second commit I added a jackpot mechine  to my project which is still not completed

---
+++ Jackpot mechine/project.js
@@ -0,0 +1,84 @@
+//1 Deposite the amount
+//2 Dertermine the number of lines to bet on
+//3 Collect the bet amount 
+//4 Spin the slot machine
+//5 Check if the user won
+//6 Give the user their winnings
+//7 Play again
+
+const prompt = require('prompt-sync')();
+
+const ROW = 3;
+const COL = 3;
+const SYMBOLS_COUNT = {
+    "A" : 2,
+    "B" : 4,
+    "C" : 6,
+    "D" : 8,
+}
+const SYMBOLS_VALUES = {
+    "A" : 5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2,
+}
+
+
+const Deposite = () => {
+    const DepositeAmount = parseFloat(prompt("Enter the amount to deposit: "));
+
+
+    if(isNaN(DepositeAmount) || DepositeAmount <= 0) {
+        console.log("Invalid deposit amount. Please enter a positive number.");
+        return Deposite();
+    }else{
+        return DepositeAmount;
+    }
+}
+
+const BetLines = () => {
+    const lines = parseInt(prompt("Enter the number of lines to bet on (1-3): "));
+    if(isNaN(lines) || lines <1 || lines > 3){
+        console.log("Invalid number of lines Please enter a number between 1 and 3. ");
+        return BetLines;
+    }else{
+        return lines;
+    }
+}
+
+const BetAmount = (Balance) => {
+    const betAmount = parseFloat(prompt(`Enter the bet amount per line (Balance: ${Balance}): `));
+    
+    if(isNaN(betAmount) || betAmount <= 0 || betAmount * lines > Balance) {
+        console.log("Invalid bet amount. Please enter a positive number that does not exceed your balance.");
+        return BetAmount(Balance);
+    } else {
+        return betAmount;
+    }
+
+}
+const Spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+
+    const reels = [];
+    for (let i = 0; i < ROW; i++) {
+        reels.push([]);
+        const symbolsCopy = [...symbols];
+        for (let j = 0; j < COL; j++) {
+            const randomIndex = Math.floor(Math.random() * symbolsCopy.length);
+            reels[j].push(symbolsCopy[randomIndex]);
+            symbolsCopy.splice(randomIndex, 1);
+        }
+        return reels;
+    }
+}
+Spin();
+
+let Balance =  Deposite();
+const lines = BetLines();
+const betAmount = BetAmount(Balance);
